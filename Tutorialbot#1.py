a4edd3ec0f25cf4629734ea1fdb7b76fa6e0a190
rainbowtheme <43517515+rainbowtheme@users.noreply.github.com>
2018-10-11 10:33:30 +0700

    Update Tutorialbot#1.py

--- Tutorialbot#1.py
+++ Tutorialbot#1.py
@@ -12,6 +12,7 @@ Client = discord.client
 client = commands.Bot(command_prefix = '!')
 Clientdiscord = discord.Client()
 client.remove_command('help')
+answers = ["apple", "ball", "cat", "dog", "elephant", "frog", "gun"]
 
 
 @client.event
@@ -24,12 +25,14 @@ async def on_member_join(member):
     role = discord.utils.get(member.server.roles, name='Members')
     await client.add_roles(member, role)
 
-@client.event
-async def on_message(message):
-    if message.content.startswith('!gen'):
-        randomlist = ['emiliogiraltnorro@hotmail.com:emilio2003','felix@methe.de:felix1505','mangysage@gmail.com:Scoobi71@','pashodidi@abv.bg:Husjern20','peyjames13@gmail.com:gobears34','philipp.rahrbach@gmx.de:Philipp2003!!...','pratt.trevor@rocketmail.com:s1937tap','enco.juricev555@gmail.com:1234enco','christenson03@gmail.com:Samuel03','perezisaiah843@gmail.com:hectorxd27','s.ribich95@gmail.com:steben01256!','rembrandt132010@gmail.com:66018530','ludvigekelof@gmail.com:kattenbosse1','martinbruraas@gmail.com:Beretta1','hellespont24@hotmail.com:Exia2012','brown_521@msn.com:Monty123','knoll1749@charter.net:Thunder523','xbungalowx@gmail.com:Jailbird2','seanjean1324@gmail.com:killerjet57']
-        await client.send_message(message.author,(random.choice(randomlist)))
-        await client.send_message(message.channel,('Check Your DM :smiley:'))
-        await client.send_message(message.author,'Bot by @Blue Gaming#5147')
+@bot.command()  
+async def choose(k : int):
+    """Chooses between multiple choices."""
+    if 0 <= k <= 50:
+        await bot.say("This is your random {} pick".format(k))
+        embed = discord.Embed(description='\n'.join(random.choices(answers, k=k)))
+        await bot.say(embed=embed)
+    else:
+        await bot.say("Invalid number")
 
 client.run(os.getenv("TOKEN"))
