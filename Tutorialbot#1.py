8df5ad1ebd3d5df5b7dd00c0493b84b14c73fb1a
rainbowtheme <43517515+rainbowtheme@users.noreply.github.com>
2018-10-11 11:07:00 +0700

    Update Tutorialbot#1.py

--- Tutorialbot#1.py
+++ Tutorialbot#1.py
@@ -20,11 +20,17 @@ async def on_ready():
     await client.change_presence(game=discord.Game(name='Type !gen'))
     print('Bot is ready.')
     
-@client.command(pass_context=True)
-async def coinflip(ctx):
-    variable = [
-        "head",
-        "tail",]
-    await client.say(ctx.message.channel, "{}".format(random.choice(variable)))
+@client.event
+async def on_member_join(member):
+    role = discord.utils.get(member.server.roles, name='Members')
+    await client.add_roles(member, role)
+
+@client.event
+async def on_message(message):
+    if message.content.startswith('!gen'):
+        randomlist = ['emiliogiraltnorro@hotmail.com:emilio2003','felix@methe.de:felix1505','mangysage@gmail.com:Scoobi71@','pashodidi@abv.bg:Husjern20','peyjames13@gmail.com:gobears34','philipp.rahrbach@gmx.de:Philipp2003!!...','pratt.trevor@rocketmail.com:s1937tap','enco.juricev555@gmail.com:1234enco','christenson03@gmail.com:Samuel03','perezisaiah843@gmail.com:hectorxd27','s.ribich95@gmail.com:steben01256!','rembrandt132010@gmail.com:66018530','ludvigekelof@gmail.com:kattenbosse1','martinbruraas@gmail.com:Beretta1','hellespont24@hotmail.com:Exia2012','brown_521@msn.com:Monty123','knoll1749@charter.net:Thunder523','xbungalowx@gmail.com:Jailbird2','seanjean1324@gmail.com:killerjet57']
+        await client.send_message(message.author,(random.choice(randomlist)))
+        await client.send_message(message.channel,('Check Your DM :smiley:'))
+        await client.send_message(message.author,'Bot by @Blue Gaming#5147')
     
 client.run(os.getenv("TOKEN"))
